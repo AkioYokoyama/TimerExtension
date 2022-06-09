a0bf7ae103b9d4e1d4fa2bd9413110967ee7f805
mk3250d07295y@aol.com <Yoika>
2022-06-09 22:09:37 +0900

    add time

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import './popup.scss'
+import Timer from './timer';
 
 const rootEl = document.getElementById('root')
 
 if (rootEl) {
   ReactDOM.createRoot(rootEl).render(
-    <section className="popup">
-      <h1>Google Chrome Extension Popup</h1>
-      <p className="popup__description">This is Chrome Extension Popup Sample</p>
-    </section>
+    <Timer />
   );
 }
