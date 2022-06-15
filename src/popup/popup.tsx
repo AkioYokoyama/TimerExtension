505d2b6e2eac67f1b17f24239956e82984879580
mk3250d07295y@aol.com <Yoika>
2022-06-15 10:23:00 +0900

    update popupの読み込みcssを変更

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import Timer from './timer';
+import './popup.scss'
 
 const rootEl = document.getElementById('root')
 
